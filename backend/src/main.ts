fc3d7e899775147ee13d8c5118374a5fab2766d4
AliNemir <alinemir283@gmail.com>
2025-08-22 22:13:01 +0300

    feat: add new origins to the cors config for production

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -17,10 +17,9 @@ async function bootstrap() {
   // Enable CORS with specific configuration for credentials
   app.enableCors({
     origin: [
-      'http://localhost:5173', 
+      'http://localhost:5173',
       'http://127.0.0.1:5173',
-      'https://easygenerator-frontend.vercel.app', // Add your frontend production URL
-      'https://easygenerator-assessment.vercel.app' // Alternative frontend URL
+      'https://easygenerator-assessment-1.onrender.com'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
@@ -66,13 +65,11 @@ async function bootstrap() {
 
   const port = process.env.PORT ?? 3000;
   const host = process.env.NODE_ENV === 'production' ? '0.0.0.0' : '127.0.0.1';
-  
+
   await app.listen(port, host);
 
   logger.info(`Application is running on: http://${host}:${port}`);
-  logger.info(
-    `Swagger documentation available at: http://${host}:${port}/api`,
-  );
+  logger.info(`Swagger documentation available at: http://${host}:${port}/api`);
 }
 
 bootstrap().catch((error) => {
